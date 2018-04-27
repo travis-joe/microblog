871ec0ae9a28195c8b625cffb47b547802e9319b
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-04-27 18:03:29 +0800

    cp.3

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
-from flask import render_template
+from flask import render_template, flash, redirect, url_for
 
 from app import app
+from app.form import LoginForm
 
 
 @app.route('/')
@@ -18,4 +19,14 @@ def index():
         }
     ]
     print(type(user))
-    return render_template('index.html', title='home', user=user,  posts=posts)
+    return render_template('index.html', title='home', user=user, posts=posts)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
