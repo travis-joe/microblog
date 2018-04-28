b34f1ff388fa9970ae16856ff08ce1f89f835b2a
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-04-28 14:11:03 +0800

    cp.4

--- config.py
+++ config.py
@@ -3,3 +3,5 @@ import os
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = 'mysql+cymysql://root:qwe123@localhost:3306/microblog'
