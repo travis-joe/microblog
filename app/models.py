3b15b3db9f2f0b92f73ccbe1d0e90401eafe877a
qzy <weelgod@gmail.com>
2018-04-30 10:31:35 +0800

    cp.6

--- app/models.py
+++ app/models.py
@@ -1,3 +1,4 @@
+from hashlib import md5
 from datetime import datetime
 from werkzeug.security import generate_password_hash, check_password_hash
 from app import db, login
@@ -15,6 +16,13 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
     posts = db.relationship('Post', backref='author', lazy='dynamic')
+    about_me = db.Column(db.String(140))
+    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def avatar(self, size):
+        digest = md5(self.email.lower().encode('utf-8')).hexdigest()
+        return 'https://www.gravatar.com/avatar/{}?d=identicon&s={}'.format(
+            digest, size)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
