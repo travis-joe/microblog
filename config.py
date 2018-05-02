1e4767b89523593fca680bc0fb8e873956bf1b46
wb-qcy313750 <wb-qcy313750@antfin.com>
2018-05-02 17:24:58 +0800

    cp.9

--- config.py
+++ config.py
@@ -11,3 +11,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     ADMINS = ['weelgod@gmail.com']
+    POSTS_PER_PAGE = 25
